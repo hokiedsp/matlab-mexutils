e84d7d20178f413433636babd33657929959db9b
Takeshi Ikuma <tikuma@gmail.com>
2018-02-25 17:58:21 -0600

    Updated documentation

--- include/mexAllocator.h
+++ include/mexAllocator.h
@@ -4,6 +4,23 @@
 #include <mex.h>
 #include <memory>
 
+/**
+ * \brief Allocator to use MATLAB memory manager
+ * 
+ * A custom C++ Allocator to use MATLAB memory manager.
+ * 
+ * \note DO NOT USE THIS ALLOCATOR BEYOND SIMPLE DATA TYPES!!
+ *       In general, the use of C-based memory management functions should
+ *       not be used in C++ Allocator class. However, it works fine as long
+ *       as it is managing a simple data type (int, double, char, etc.).
+ *       There may be unforeseen consequence if this class is used to allocate
+ *       memory for a struct or class.
+ * 
+ * \note Intended Application: Use this Allocator for a generic C++ template
+ *       library which must dynamically allocate memory but also releases the
+ *       allocated memory to the user, which is under a specific memory management
+ *       regime (i.e., MATLAB) to avoid copying.
+ */
 template <typename T>
 class mexAllocator : public std::allocator<T>
 {
