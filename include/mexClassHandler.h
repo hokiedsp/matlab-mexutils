6aed4e36099c75f8a2aa16ae749ecc4ef78e8f03
Takeshi Ikuma <tikuma@gmail.com>
2018-02-24 23:02:36 -0600

    mexClassHandle to contain wrapped class object; completed documentation

--- include/mexClassHandler.h
+++ include/mexClassHandler.h
@@ -5,46 +5,184 @@
 
 #include <mex.h>
 #include <stdint.h>
+#include <typeinfo>
 
 #define CLASS_HANDLE_SIGNATURE 0xFF00F0A5
-template <class base>
+template <class wrappedClass>
 class mexClassHandle
 {
 public:
   /**
-   *\brief Access the managing C++ object pointer
+   *\brief Access the wrapped C++ object
    *
-   * Returns a pointer to the managed object or nullptr if no object is owned. 
+   * Returns a reference to the managed object or nullptr if no object is owned. 
    * 
-   *\returns a pointer to the managed object or nullptr if no object is owned. 
+   *\returns a reference to the managed object or nullptr if no object is owned. 
    */
-  base *get() { return ptr_m; }
+  wrappedClass &get() { return obj_m; }
 
   /**
-   *\brief Create new mexClassHandle
+   *\brief Instantiate the wrapped class and return its wrapper mxArray
    *
-   * All mexClassHandle objects must be created with this function. The
-   * function returns a pointer to a new mxArray object, which contains
-   * the new mexClassHandle object. To access the mexClassHandle object,
-   * use \ref getHandle(). To access the managed C++ object, use \ref
-   * getObject().
+   * Create() function accepts variable arguments, which are then passed 
+   * directly to the constructor of the target class, instantiating the
+   * object wrapped by the mxArray.
    * 
-   * Calling this function locks the MEX function (i.e., shared library) 
-   * in memory until \ref destroy() is called on this object. Multiple
-   * mexClassHandles may be employed in an MEX function as the lock is
-   * counted; the MEX function won't be freed until all locks are released.
+   * The created mxArray encompasses a scalar uint64 value, which is a cast
+   * of the pointer to a mexClassHandle class instance. Before the returned
+   * mxArray is destroyed (either in MATLAB or in C++), \ref destroy() or 
+   * \ref _destroy() must be called to delete the pointed mexClassHandle object.
    * 
-   * \param[in] ptr A pointer to an unmanaged C++ object
+   * \param[in] args Template parameter pack of arguments to be passed on to 
+   *                 the constructor of the wrapped class.
    * \returns mxArray containing the pointer to the mexClassHandle which
-   *          manages \ref ptr.
+   *          wraps the instantiated wrapped class.
    */
-  static mxArray *create(base *ptr)
+  template <class... Args>
+  static mxArray *create(Args... args)
   {
     mexLock();
     mxArray *out = mxCreateNumericMatrix(1, 1, mxUINT64_CLASS, mxREAL);
-    *((uint64_t *)mxGetData(out)) = reinterpret_cast<uint64_t>(new mexClassHandle<base>(ptr));
+    *((uint64_t *)mxGetData(out)) = reinterpret_cast<uint64_t>(new mexClassHandle<wrappedClass>(args...));
     return out;
   }
+
+  /**
+   *\brief Copy-construct the wrapped class instace and return its wrapper mxArray
+   *
+   * This create() function copy constructs the wrapped class instance, and creates 
+   * returns the wrapping mxArray object.
+   * 
+   * The created mxArray encompasses a scalar uint64 value, which is a cast
+   * of the pointer to a mexClassHandle class instance. Before the returned
+   * mxArray is destroyed (either in MATLAB or in C++), \ref destroy() or 
+   * \ref _destroy() must be called to delete the pointed mexClassHandle object.
+   * 
+   * \param[in] src Source class object.
+   * \returns mxArray containing the pointer to the mexClassHandle which
+   *          wraps the copied wrapped class instance.
+   */
+  static mxArray *create(const wrappedClass &src)
+  {
+    mexLock();
+    mxArray *out = mxCreateNumericMatrix(1, 1, mxUINT64_CLASS, mxREAL);
+    *((uint64_t *)mxGetData(out)) = reinterpret_cast<uint64_t>(new mexClassHandle<wrappedClass>(src));
+    return out;
+  }
+
+  /**
+   *\brief Move-construct the wrapped class instace and return its wrapper mxArray
+   *
+   * This create() function move constructs the wrapped class instance, and creates 
+   * returns the wrapping mxArray object.
+   * 
+   * The created mxArray encompasses a scalar uint64 value, which is a cast
+   * of the pointer to a mexClassHandle class instance. Before the returned
+   * mxArray is destroyed (either in MATLAB or in C++), \ref destroy() or 
+   * \ref _destroy() must be called to delete the pointed mexClassHandle object.
+   * 
+   * \param[in] src Source class object.
+   * \returns mxArray containing the pointer to the mexClassHandle which
+   *          wraps the copied wrapped class instance.
+   */
+  static mxArray *create(wrappedClass &&src)
+  {
+    mexLock();
+    mxArray *out = mxCreateNumericMatrix(1, 1, mxUINT64_CLASS, mxREAL);
+    *((uint64_t *)mxGetData(out)) = reinterpret_cast<uint64_t>(new mexClassHandle<wrappedClass>(src));
+    return out;
+  }
+
+  /**
+   * \brief Get wrapped class object from mxArray
+   * 
+   * This function returns a reference to the wrapped class object in the
+   * given mxArray and returns it after a series of successful validation.
+   * 
+   * /param[in] in Pointer to wrapper mxArray object
+   * /returns a reference to the managing mexClassHandle object.
+   * 
+   * /throws mexRuntimeError if mxArray does not own a mexClassHandle
+   */
+  static wrappedClass &getObject(const mxArray *in)
+  {
+    return mexClassHandle<wrappedClass>::getHandle(in)->obj_m;
+  }
+
+ /**
+ * \brief Destruct wrapped class instance (unsafe version)
+ * 
+ * Destruct mexClassHandle pointed by the mxArray. This function
+ * leaves mxArray data unchanged, so care must be taken after calling this function
+ * to clear the mxArray content.
+ * 
+ * /param[in] in Pointer to wrapper mxArray object
+ */
+  static void _destroy(const mxArray *in)
+  {
+    delete mexClassHandle<wrappedClass>::getHandle(in);
+
+    // allow MATLAB to release the MEX function
+    mexUnlock();
+  }
+
+  /**
+   * \brief Destruct wrapped class instance
+   * 
+   * Destruct mexClassHandle pointed by the mxArray and clear the content
+   * of mxArray.
+   * 
+   * /param[in] in Pointer to wrapper mxArray object
+   */
+  static void destroy(mxArray *in)
+  {
+    // call the const mxArray version to perform the destruction of the object
+    _destroy(in);
+
+    // then also clear the mxArray to be an empty array
+    const mwSize dims[2] = {0, 0};
+    mxSetData(in, NULL);
+    mxSetDimensions(in, dims, 2);
+  }
+
+private:
+  /**
+   * \brief mexClassHandle constructor 
+   * 
+   * Instantiates the wrapped class with given arguments.
+   * 
+   * /param[in] args Variable arguments for the wrapped class construction
+   */
+  template <class... Args>
+  mexClassHandle(Args... args) : obj_m(args...), name_m(typeid(wrappedClass).name()) {}
+
+  /**
+   * \brief mexClassHandle construction, copy-constructing wrapped class
+   * 
+   * Instantiates the wrapped class by copy construction.
+   * 
+   * /param[in] src Source wrapped class object
+   */
+  mexClassHandle(const wrappedClass *src) : obj_m(src), name_m(typeid(wrappedClass).name()) {}
+
+  /**
+   * \brief mexClassHandle construction, move-constructing wrapped class
+   * 
+   * Instantiates the wrapped class by move construction.
+   * 
+   * /param[in] src Source wrapped class object
+   */
+    mexClassHandle(wrappedClass &&src) : obj_m(src), name_m(typeid(wrappedClass).name()) {}
+
+  /**
+   * \brief mexClassHandle destruction
+   * 
+   * Clears name to make sure an attempt to access after _destroy() call yields in exception
+   */
+  ~mexClassHandle() { name_m = NULL; }
+
+  const char* name_m; // points to the class
+  wrappedClass obj_m; // instance of the wrapped class
 
   /**
    * \brief Get mexClassHandle from mxArray
@@ -54,86 +192,70 @@ public:
    * 
    * /param[in] in A pointer to an mxArray object
    * /returns a reference to the managing mexClassHandle object.
+   * 
+   * /throws mexRuntimeError if mxArray does not own a mexClassHandle
    */
-  static mexClassHandle<base> &getHandle(const mxArray *in)
+  static mexClassHandle<wrappedClass> *getHandle(const mxArray *in)
   {
     if (mxGetNumberOfElements(in) != 1 || mxGetClassID(in) != mxUINT64_CLASS || mxIsComplex(in))
       throw mexRuntimeError("invalidMexObjectHandle", "Input must be a real uint64 scalar.");
-    mexClassHandle<base> &ptr = *reinterpret_cast<mexClassHandle<base> *>(*((uint64_t *)mxGetData(in)));
-    if (!ptr.isValid())
+    mexClassHandle<wrappedClass> *ptr = reinterpret_cast<mexClassHandle<wrappedClass> *>(*((uint64_t *)mxGetData(in)));
+
+    //  if (ptr->name_m!=typeid(wrappedClass).name()) // <-likely safe but not standard guaranteed alternative
+    if (!ptr->name_m || strcmp(ptr->name_m, typeid(wrappedClass).name()))
       throw mexRuntimeError("invalidMexObjectHandle", "Handle not valid.");
+
     return ptr;
   }
-
-  /**
-   * \brief Get managed class object from mxArray
-   * 
-   * This function directly obtains a pointer to the managed class object in the
-   * given mxArray and returns it after a series of successful validation.
-   * 
-   * /param[in] in A pointer to an mxArray object
-   * /returns a pointer to the managing mexClassHandle object.
-   */
-  static base *getObject(const mxArray *in)
-  {
-    return mexClassHandle<base>::getHandle(in).ptr_m;
-  }
-
-  static void destroy(const mxArray *in, bool delete_obj = true)
-  {
-    mexClassHandle<base> &ref = mexClassHandle<base>::getHandle(in);
-    if (!delete_obj)
-      ref.ptr_m = NULL;
-
-    // destruct the underlying C++ class instance
-    delete &ref;
-
-    // allow MATLAB to release the MEX function
-    mexUnlock();
-  }
-
-private:
-  mexClassHandle(base *ptr) : ptr_m(ptr), name_m(typeid(base).name()) { signature_m = CLASS_HANDLE_SIGNATURE; }
-  ~mexClassHandle()
-  {
-    signature_m = 0;
-    if (ptr_m) delete ptr_m;
-  }
-  bool isValid() { return ((signature_m == CLASS_HANDLE_SIGNATURE) && !strcmp(name_m.c_str(), typeid(base).name())); }
-
-  uint32_t signature_m;
-  std::string name_m;
-  base *ptr_m;
 };
 
-/// mexClassHandler
-/// Function to handle MATLAB class member mex-function calls. The associated MATLAB class must have a (private)
-/// property named 'backend' which stores the C++ class object handle (must inherit mexClassHandle).
-///
-/// In Matlab, the associated class object must have a private property 'backend' to hold a mexClassHandle object as well as
-/// a private STATIC method (method's name does not matter, we assume 'mexfcn' in this document).
-///
-/// mexClass C++ class must provide get_classname() static member function which returns the associated Matlab class name
-/// for verification purpose
-///
-/// There are three basic types of calls from MATLAB (assuming the MATLAB class name to be MATCLASS and its object name to be OBJ):
-/// * MATCLASS.mexfcn(OBJ,varargin)
-///   This calls the backend class constructor if OBJ.backend property is not populated and populates it with the C++ object handle.
-///   the second argument maybe omitted.
-/// * varargout = MATCLASS.mexfcn(OBJ,'action',varargin)
-///   This implements dynamic class action. OBJ is expected to be a scalar entity (if not only the first element is used)
-/// * varargout = MATCLASS.mexfcn('action',varargin)
-///   This implements static class action.
-///
-/// There are three preset dynamic actions:
-/// * action='delete' : destructor
-/// * action='get'    : property get : implemented by mexClass::get_prop
-/// * action='set'    : property set : implemented by mexClass::set_prop
-/// * custom actions  :                implemented by mexClass::action_handler
+/**
+ * \brief Function to implement MATLAB class' C++ interface member function
+ * 
+ * mexClassHandler may be a sole function call from mexFunction() to interface the MATLAB 
+ * and C++ classes. The mexFunction should implement a private static member function of the 
+ * wrapping MATLAB class, and the MATLAB class must have a (private) property named 
+ * 'backend' which receives the C++ class object handle using the \ref mexClassHandle mechanism.
+ * 
+ * The arguments are exactly the same as mexFunction() and its arguments could be directly 
+ * passed down to mexClassHandler():
+ * 
+ * \param[in]    nlhs Number of expected output mxArrays
+ * \param[inout] plhs Array of pointers to the expected output mxArrays
+ * \param[in]    nrhs Number of input mxArrays
+ * \param[in]    prhs Array of pointers to the input mxArrays.
+ * 
+ * The mexFunction based on this function interact directly with the wrapping MATLAB class, via 
+ * 4 basic MATLAB function signature:
+ * 
+ * * mexfcn(obj,varargin)          - Create a new C++ class instance and store it as a `backend` 
+ *                                   MATLAB class property.
+ * * mexfcn(obj,'delete')          - Destruct the C++ class instance.
+ * * mexfcn(obj,'action',varargin) - Perform an action
+ * * mexfcn('action',varargin)     - Perform a static action
+ * 
+ * Note that all non-static action signature receives the MATLAB object, enabling the C++ class
+ * object to interact with MATLAB class object as needed.
+ * 
+ * The template class `mexClass` must either inherit \ref mexFunctionClass or match its public 
+ * member function syntax to be compatible with this function. Three member functions it must 
+ * provide must have the signatures:
+ * 
+ * * static std::string get_classname();
+ * * bool action_handler(const mxArray *mxObj, const std::string &action, 
+ *                       int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
+ * * static bool static_handler(std::string action, int nlhs, mxArray *plhs[], 
+ *                              int nrhs, const mxArray *prhs[]);
+ * Also, its constructor must be compatible with the signature:
+ * 
+ *    constructor(int nrhs, const mxArray *prhs[]);
+ * 
+ * where nrhs & prhs accounts for `varargin` inputs of the create-new mexFunction call.
+ */
 template <class mexClass>
 void mexClassHandler(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  std::string class_name = mexClass::get_classname(); // associated matlab class
+  std::string class_name = mexClass::get_classname(); // should match the associated matlab class
 
   try
   {
@@ -178,28 +300,9 @@ void mexClassHandler(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         // no backend object, create a new
         if (nlhs > 1)
           throw mexRuntimeError(class_name + ":tooManyOutputArguments", "Only one argument is returned for object construction.");
-        mexClass *ptr(NULL);
-        try
-        {
-          ptr = new mexClass(nrhs - 1, prhs + 1);
-          if (ptr == NULL)
-            throw mexRuntimeError(class_name + ":mex:constructorFail", "Constructor failed silently.");
-        }
-        catch (mexRuntimeError &e)
-        {
-          std::string id = e.id();
-          if (id.empty()) // no id given
-            throw mexRuntimeError(class_name + ":mex:constructorFail", e.what());
-          else
-            throw mexRuntimeError(class_name + ":mex:" + e.id(), e.what());
-        }
-        catch (std::exception &e)
-        {
-          throw mexRuntimeError(class_name + ":mex:constructorFail", e.what());
-        }
 
         // set backend with the pointer to the newly created C++ object
-        mxSetProperty((mxArray *)prhs[0], 0, "backend", mexClassHandle<mexClass>::create(ptr));
+        mxSetProperty((mxArray *)prhs[0], 0, "backend", mexClassHandle<mexClass>::create(nrhs - 1, prhs + 1));
       }
       else if (nrhs < 2 || !mxIsChar(prhs[1]))
       {
@@ -211,7 +314,7 @@ void mexClassHandler(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         std::string action = mexGetString(prhs[1]);
 
         // get the C++ object (throws exception if invalid)
-        mexClass *obj = mexClassHandle<mexClass>::getObject(backend);
+        mexClass &obj = mexClassHandle<mexClass>::getObject(backend);
 
         // check for the delete action
         if (action == "delete")
@@ -222,7 +325,7 @@ void mexClassHandler(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         {
           try
           {
-            if (!obj->action_handler(prhs[0], action, nlhs, plhs, nrhs - 2, prhs + 2))
+            if (!obj.action_handler(prhs[0], action, nlhs, plhs, nrhs - 2, prhs + 2))
             {
               std::string msg("Unknown action: ");
               throw mexRuntimeError(class_name + ":unknownAction", msg + action);
@@ -252,12 +355,36 @@ void mexClassHandler(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   }
 }
 
-// Base class for an internal MEX class interfaced a MATLAB class object using mexClassHandle class
+/**
+ * \brief A near-compatible base class for mexClassHandler function
+ * 
+ * This class could be used as the base class or as the template for a custom class
+ * to be used with \ref mexClassHandler() function. It processes 4 basic actions: set, 
+ * get, save, and load. Each of which must be implemented in the derived class.
+ * 
+ * MATLAB signature for these actions are:
+ * 
+ * * value = mexfcn(obj,'get',name)
+ * * mexfcn(obj,'set',name,value)
+ * * data = mexfcn(obj,'save')
+ * * mexfcn(obj,'load',data)
+ * 
+ * Note that this class misses the necessary static functions: get_classname() and 
+ * static_handler(). They must also be implemented in the derived class.
+*/
 class mexFunctionClass
 {
 public:
-  // static std::string get_classname() { return "mexClassGeneric"; };
-
+/**
+ * \brief  Perform one of basic class actions
+ * 
+ * \param[in]    mxObj  Associated MATLAB class object
+ * \param[in]    action Name of the action to be performed
+ * \param[in]    nlhs   Number of expected output mxArrays
+ * \param[inout] plhs   Array of pointers to the expected output mxArrays
+ * \param[in]    nrhs   Number of input mxArrays
+ * \param[in]    prhs   Array of pointers to the input mxArrays.
+ */
   virtual bool action_handler(const mxArray *mxObj, const std::string &action, int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   {
     if (action == "set") // (H,"set","name",value)
@@ -316,15 +443,38 @@ public:
     return true;
   }
 
-  static bool static_handler(std::string action, int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-  {
-    return false;
-  }
-
 protected:
+/**
+ * \brief  Set a property value
+ * 
+ * \param[in]    mxObj  Associated MATLAB class object
+ * \param[in]    name   Name of the property
+ * \param[in]    value  New property value
+ */
   virtual void set_prop(const mxArray *mxObj, const std::string name, const mxArray *value) = 0;
+
+/**
+ * \brief  Get a property value
+ * 
+ * \param[in]    mxObj  Associated MATLAB class object
+ * \param[in]    name   Name of the property
+ * \returns the property value
+ */
   virtual mxArray *get_prop(const mxArray *mxObj, const std::string name) = 0;
 
+/**
+ * \brief  Save C++ object's data
+ * 
+ * \param[in]    mxObj  Associated MATLAB class object
+ * \returns mxArray containing the C++ object states
+ */
   virtual mxArray *save_prop(const mxArray *mxObj) { return NULL; };
-  virtual void load_prop(const mxArray *mxObj, const mxArray *value){};
+
+/**
+ * \brief  Load C++ object's data
+ * 
+ * \param[in] mxObj  Associated MATLAB class object
+ * \param[in] data   Data to reestablish the C++ object states
+ */
+  virtual void load_prop(const mxArray *mxObj, const mxArray *data){};
 };
