23a12fd800a86b74abb09fd188bfd0b19abb8fb7
Kesh Ikuma <hokiedsp@users.noreply.github.com>
2018-02-26 21:35:10 -0600

    Renamed

--- +mexcpp/BaseClass.m
+++ +mexcpp/BaseClass.m
@@ -2,20 +2,20 @@ classdef (Abstract) BaseClass < handle
 %mexcpp.BaseClass   Base Matlab class to wrap C++ class instance
 %
 %   An abstract base class to wrap C++ backend class instance. The user backend
-%   C++ class shall inherit mexFunctionClass derived in include/mexClassHandler.h.
+%   C++ class shall inherit mexFunctionClass derived in include/mexObjectHandler.h.
 %   This Matlab base class serves to provide a concrete foundation to interact
 %   with the C++ class, implementing common functions such as backend object
 %   construction and destruction, set/get backend object properties, and
 %   save/load backend data.
 %
-%   The header file 'include/mexClassHandler.h' defines 3 key elements to interact
-%   with this Matlab class: mexClassHandle, mexClassHandler, and mexFunctionClass.
+%   The header file 'include/mexObjectHandler.h' defines 3 key elements to interact
+%   with this Matlab class: mexObjectHandle, mexObjectHandler, and mexFunctionClass.
 %
-%   The mexClassHandle is based on Oliver Woodford's class wrapper
+%   The mexObjectHandle is based on Oliver Woodford's class wrapper
 %   (https://www.mathworks.com/matlabcentral/fileexchange/38964), which provides
 %   the way to store C++ pointer as a Matlab uint64 variable.
 %
-%   The mexClassHandler template function requires to be specified for the user's
+%   The mexObjectHandler template function requires to be specified for the user's
 %   C++ class should be the only line in the user's mexFunction(), receiving all
 %   the arguments as is. This function accepts the following Matlab syntax:
 %
