83549181a1214b4bce6ffcfffc4aa2a2cfdc9504
Takeshi Ikuma <tikuma@gmail.com>
2019-07-07 16:20:43 -0500

    Removed inheriting std::allocator

--- include/mexAllocator.h
+++ include/mexAllocator.h
@@ -1,38 +1,37 @@
 #pragma once
 
 // CUSTOM STL allocator using the Intel MKL memory management functions
-#include <mex.h>
 #include <memory>
+#include <mex.h>
 
 /**
  * \brief Allocator to use MATLAB memory manager
- * 
+ *
  * A custom C++ Allocator to use MATLAB memory manager.
- * 
+ *
  * \note DO NOT USE THIS ALLOCATOR BEYOND SIMPLE DATA TYPES!!
  *       In general, the use of C-based memory management functions should
  *       not be used in C++ Allocator class. However, it works fine as long
  *       as it is managing a simple data type (int, double, char, etc.).
  *       There may be unforeseen consequence if this class is used to allocate
  *       memory for a struct or class.
- * 
+ *
  * \note Intended Application: Use this Allocator for a generic C++ template
  *       library which must dynamically allocate memory but also releases the
- *       allocated memory to the user, which is under a specific memory management
- *       regime (i.e., MATLAB) to avoid copying.
+ *       allocated memory to the user, which is under a specific memory
+ * management regime (i.e., MATLAB) to avoid copying.
  */
-template <typename T>
-class mexAllocator : public std::allocator<T>
+#include <type_traits>
+template <typename T> struct mexAllocator
 {
-public:
-  mexAllocator() {}
-  mexAllocator &operator=(const mexAllocator &rhs)
-  {
-    std::allocator<T>::operator=(rhs);
-    return *this;
-  }
-
-  pointer allocate(size_type n, const void *hint = NULL)
+  template <typename U> friend struct mexAllocator;
+  using value_type = T;
+  using pointer = T *;
+  using propagate_on_container_copy_assignment = std::true_type;
+  using propagate_on_container_move_assignment = std::true_type;
+  using propagate_on_container_swap = std::true_type;
+  template <typename U> mexAllocator() {}
+  pointer allocate(std::size_t n)
   {
     pointer p = NULL;
     if (!hint)
@@ -46,19 +45,13 @@ public:
     }
     return p;
   }
-
-  void deallocate(pointer p, size_type n)
-  {
-    mxFree(p);
-  }
-
-  void construct(pointer p, const T &val)
-  {
-    new (p) T(val);
-  }
-
-  void destroy(pointer p)
-  {
-    p->~T();
-  }
+  void deallocate(pointer p, std::size_t n) { mxFree(p); }
+  // template <typename U> bool operator==(ArenaAllocator<U> const &rhs) const
+  // {
+  //   return arena == rhs.arena;
+  // }
+  // template <typename U> bool operator!=(ArenaAllocator<U> const &rhs) const
+  // {
+  //   return arena != rhs.arena;
+  // }
 };
